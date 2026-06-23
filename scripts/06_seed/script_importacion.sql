a20f81d7bb154ba2d2b245ee4d5c332e22a5bad4
Lucas Acuña <lucas6haedo@gmail.com>
2026-06-23 13:39:39 -0300

    Modifico lectura del SIB a nativa de XLSX

--- scripts/06_seed/script_importacion.sql
+++ scripts/06_seed/script_importacion.sql
@@ -11,12 +11,17 @@
                             Carga los CSV con BULK INSERT, procesa
                             y muestra el log de resultados.
 
-    - SIB:  https://sib.gob.ar/areas-protegidas  (XLSX convertido a CSV)
+    - SIB:  https://sib.gob.ar/areas-protegidas    (XLSX nativo via OPENROWSET)
     - CIAM: https://ciam.ambiente.gob.ar/repositorio.php?tid=5
 
-    PREREQUISITO:
-    Ajustar @rutaBase a la ubicacion del repo en su PC.
-    El path debe terminar con barra invertida.
+    PREREQUISITOS:
+    1. Driver Microsoft.ACE.OLEDB.12.0 instalado.
+       Descarga: https://www.microsoft.com/en-us/download/details.aspx?id=54920
+    2. Habilitar Ad Hoc Distributed Queries (una sola vez):
+       EXEC sp_configure 'show advanced options', 1; RECONFIGURE;
+       EXEC sp_configure 'Ad Hoc Distributed Queries', 1; RECONFIGURE;
+    3. Ajustar @rutaBase a la ubicacion del repo en tu PC.
+       El path debe terminar con barra invertida.
 */
 
 USE GestionParquesNacionales;
@@ -28,7 +33,7 @@ GO
 -- ===========================================================
 DECLARE @rutaBase VARCHAR(500) = 'C:\Users\Lucas\Desktop\facu\bda\TP\TP-BDDA-ParquesNacionales\scripts\06_seed\datasets\';
 
-DECLARE @rutaSib VARCHAR(500) = @rutaBase + 'sib_areas_protegidas.csv';
+DECLARE @rutaSib VARCHAR(500) = @rutaBase + 'sib_areas_protegidas.xlsx';
 DECLARE @rutaCiam VARCHAR(500) = @rutaBase + 'ciam_superficies.csv';
 DECLARE @sql NVARCHAR(MAX);
 
@@ -42,18 +47,36 @@ TRUNCATE TABLE Gestion.logImportacion;
 
 
 -- ===========================================================
--- PASO 1: cargar el CSV del SIB en su staging
+-- PASO 1: leer el XLSX del SIB con OPENROWSET y cargar en staging
+-- El XLSX tiene un titulo en fila 1 y encabezados en fila 2, por eso
+-- el rango arranca en A3.
 -- ===========================================================
 SET @sql = '
-BULK INSERT Gestion.stagingSib
-FROM ''' + @rutaSib + '''
-WITH (
-    FIELDTERMINATOR = '';'',
-    ROWTERMINATOR = ''0x0A'',
-    CODEPAGE = ''65001'',         -- UTF-8
-    FIRSTROW = 1,                 -- No tiene encabezado (lo sacamos al convertir)
-    FIELDQUOTE = ''"''            -- SQL Server 2017+
-);';
+INSERT INTO Gestion.stagingSib 
+    (provincia, nombreCompleto, anioCreacion, region, superficie, latitud, longitud, 
+     leyCreacion, ecorregiones, categoriaInternacional, 
+     especiesRegistradas, animales, bacterias, hongos, plantas)
+SELECT 
+    CAST(F1 AS VARCHAR(100))  AS provincia,
+    CAST(F2 AS VARCHAR(200))  AS nombreCompleto,
+    CAST(F3 AS VARCHAR(20))   AS anioCreacion,
+    CAST(F4 AS VARCHAR(100))  AS region,
+    CAST(F5 AS VARCHAR(30))   AS superficie,
+    CAST(F6 AS VARCHAR(30))   AS latitud,
+    CAST(F7 AS VARCHAR(30))   AS longitud,
+    CAST(F8 AS VARCHAR(100))  AS leyCreacion,
+    CAST(F9 AS VARCHAR(200))  AS ecorregiones,
+    CAST(F10 AS VARCHAR(200)) AS categoriaInternacional,
+    CAST(F11 AS VARCHAR(20))  AS especiesRegistradas,
+    CAST(F12 AS VARCHAR(20))  AS animales,
+    CAST(F13 AS VARCHAR(20))  AS bacterias,
+    CAST(F14 AS VARCHAR(20))  AS hongos,
+    CAST(F15 AS VARCHAR(20))  AS plantas
+FROM OPENROWSET(
+    ''Microsoft.ACE.OLEDB.12.0'',
+    ''Excel 12.0;Database=' + @rutaSib + ';HDR=NO'',
+    ''SELECT * FROM [Sheet1$A3:O1000]''
+) AS xlsx;';
 EXEC sp_executesql @sql;
 
 SELECT TOP 5 * FROM Gestion.stagingSib;
